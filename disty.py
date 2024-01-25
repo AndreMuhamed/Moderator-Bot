de69ba099f6e5711efbd94e65d6f05d31b4b4a5b
Muhamed Blogs <128980327+AndreMuhamed@users.noreply.github.com>
2024-01-25 14:48:39 +0200

    Update disty.py

--- disty.py
+++ disty.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands вооаввоопопопопо
+from discord.ext import commands вооаввоопопопопоашш
 
 bot = commands.Bot(command_prefix='!') # Set the command prefix to "!"
 
