848dca978f612b5039de66a2b229155b98be582f
Muhamed Ads <128980327+AndreMuhamed@users.noreply.github.com>
2024-11-28 15:41:26 +0200

    Update disty.py

--- disty.py
+++ disty.py
@@ -1,36 +1,34 @@
 import discord
-from discord.ext import commands 
+from discord.ext import commands
+from discord.ext.commands import has_permissions
 
-bot = commands.Bot(command_prefix='!') # Set the command prefix to "!"
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f'Logged in as {bot.user}')
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('Pong!')
+    await ctx.send("Pong!")
 
 @bot.command()
-@commands.has_permissions(kick_members=True)
+@has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason=None):
     await member.kick(reason=reason)
-    await ctx.send(f'{member.mention} has been kicked.')
+    await ctx.send(f"{member} був виключений за причиною: {reason}")
 
 @bot.command()
-@commands.has_permissions(ban_members=True)
+@has_permissions(ban_members=True)
 async def ban(ctx, member: discord.Member, *, reason=None):
     await member.ban(reason=reason)
-    await ctx.send(f'{member.mention} has been banned.')
+    await ctx.send(f"{member} був заблокований за причиною: {reason}")
 
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith('!rules'):
-        await message.channel.send('Please follow the rules!')
-
+    if message.content.startswith("!rules"):
+        await message.channel.send("Будь ласка, дотримуйтесь правил чату!")
     await bot.process_commands(message)
 
-bot.run('TOKEN')
+bot.run("YOUR_TOKEN")
